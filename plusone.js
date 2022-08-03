4bb2d7213047bf8a60651e60f37bc2c0785f9b89
devgab2 <gabsouza.dev26@gmail.com>
2022-08-03 08:45:39 -0300

    Resolução Final

--- plusone.js
+++ plusone.js
@@ -1,47 +1,39 @@
 var plusOne = function(digits) {
-    console.log("Tamanho do Array:", digits.length);
-    console.log("Último index:", digits[digits.length - 1]);
+    if(digits[digits.length - 1] === 9) {
+        let valDigit = digits.length - 1;
+        let nineQt = 0;
+        let numComp = false;
 
-    if(digits.length < 16) {
-        digits = Number(digits.join("")) + 1;
-        digits = String(digits).split("");
+        // Procura se existem outros noves atecedendo o último
+        while(numComp != true) {
+            if(digits[valDigit] === 9) {
+                digits.pop();
+                nineQt++;
+                valDigit--;
+            } else {
+                numComp = true;
+            }
+        }
+
+        // Confere se a array não está vazia depois da subtração de indexes
+        if(digits.length == 0 ) {
+            digits.push(1);
+        } else {
+            digits[digits.length - 1] = digits[digits.length - 1] + 1;
+        }
+
+        // Adiciona zeros correspondentes a quantidade de noves
+        for(let add = 0; add < nineQt; add++) {
+            digits.push(0);
+        }
+
+        return digits;
     } else {
-        let pointInit = Math.round(((digits.length - 1) / 2) - 1);
-        let pointFinal = digits.length - 1;
-        let tamArr = pointFinal - pointInit;
-        let arr = [];
-
-        // Se o tamanho da array ainda for maior que 16, divide a diferença por 2
-        if(tamArr > 16) {
-            pointInit = Math.round((pointInit + pointFinal) / 2);
-            tamArr = pointFinal - pointInit;
-        }
-
-        for(pointInit; pointInit < digits.length; pointInit++) {
-            arr.push(digits[pointInit]);
-        }
-
-        arr = Number(arr.join("")) + 1;
-        arr = String(arr).split("");
-
-        while(tamArr != 0) {
-            digits[pointFinal] = arr[tamArr];
-            tamArr--;
-            pointFinal--;
-        }
-
-        if(digits[digits.length - 1] == undefined) {
-            digits.pop();
-        }
+        digits[digits.length - 1] = digits[digits.length - 1] + 1;
+        return digits;
     }
-
-    return digits;
 }
 
-var result = plusOne([5,9,0,8,4,0,2,3,5,5,7,0,0,3,1,3,7,2,4,8,8,5,0,6,1,1,2]);
-    
+var result = plusOne([1,2,3,4,5,6,7,8,9,9,9]);
 console.log(result);
-// Result:          [6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,4]
-
-// Next Input:      [7,2,8,5,0,9,1,2,9,5,3,6,6,7,3,2,8,4,3,7,9,5,7,7,4,7,4,9,4,7,0,1,1,1,7,4,0,0,6]
-// Expectet:        [7,2,8,5,0,9,1,2,9,5,3,6,6,7,3,2,8,4,3,7,9,5,7,7,4,7,4,9,4,7,0,1,1,1,7,4,0,0,7]
+// output: [1,2,3,4,5,6,7,9,0,0,0]
